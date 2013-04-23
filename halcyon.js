debb8017446643d11a813c1571c5c7c5d57a39c9
Ben Howdle <benhowdle89@gmail.com>
2013-04-24 00:10:12 +0100

    vendor prefix init

--- halcyon.js
+++ halcyon.js
@@ -1,8 +1,7 @@
 var Halcyon = Halcyon || (function(win, doc) {
 
-    var cache = {},
+    var cache = {}, applyCSS,
     initialize = function(opts) {
-
         if (!opts.element) {
             throw new Error('You must provide a DOM element');
         }
@@ -10,6 +9,23 @@ var Halcyon = Halcyon || (function(win, doc) {
         cache.easing = opts.easing || 'ease-in-out';
         cache.speed = opts.speed + 's' || '0.5s';
         cache.interval = opts.interval || 2000;
+        cache.vendor = utils.vendor();
+
+        var vendors = ['moz', 'webkit', 'o', 'ms', ''];
+
+        applyCSS = function(opts) {
+            switch (opts.rule) {
+                case 'timing':
+                    opts.el.style[cache.vendor+'TimingFunction'] = opts.value;
+                    break;
+                case 'duration':
+                    opts.el.style[cache.vendor+'TransitionDuration'] = opts.value;
+                    break;
+                case 'transform':
+                    opts.el.style[cache.vendor+'Transform'] = opts.value;
+                    break;
+            }
+        };
 
         var wrapper = cache.element.children[0],
             ul = wrapper.children[0],
@@ -18,7 +34,7 @@ var Halcyon = Halcyon || (function(win, doc) {
         ul.appendChild(firstLI.cloneNode(true));
 
         cache.numSlides = ul.children.length;
-        cache.sliderWidth = parseInt(win.getComputedStyle(firstLI).width, 10);
+        cache.sliderWidth = firstLI.getBoundingClientRect().width || firstLI.offsetWidth;
         cache.wrapper = wrapper;
         cache.currentSlide = 1;
 
@@ -29,12 +45,38 @@ var Halcyon = Halcyon || (function(win, doc) {
     utils = {
         transition: {
             on: function() {
-                cache.wrapper.style.webkitTransitionDuration = cache.speed;
-                cache.wrapper.style.webkitTransitionTimingFunction = cache.easing;
+                applyCSS({
+                    el: cache.wrapper,
+                    rule: 'duration',
+                    value: cache.speed
+                });
+                applyCSS({
+                    el: cache.wrapper,
+                    rule: 'timing',
+                    value: cache.easing
+                });
             },
             off: function() {
-                cache.wrapper.style.webkitTransitionDuration = '';
-                cache.wrapper.style.webkitTransitionTimingFunction = '';
+                applyCSS({
+                    el: cache.wrapper,
+                    rule: 'duration',
+                    value: 0
+                });
+                applyCSS({
+                    el: cache.wrapper,
+                    rule: 'timing',
+                    value: 0
+                });
+            }
+        },
+        vendor: function() {
+            var tmp = document.createElement("div"),
+                prefixes = 'webkit Moz O ms'.split(' '),
+                i;
+            for (i in prefixes) {
+                if (typeof tmp.style[prefixes[i] + 'Transition'] != 'undefined') {
+                    return prefixes[i];
+                }
             }
         }
     },
@@ -46,13 +88,21 @@ var Halcyon = Halcyon || (function(win, doc) {
                 setTimeout(function() {
                     utils.transition.off();
                     cache.currentSlide = 2;
-                    cache.wrapper.style.webkitTransform = "translate3d(0, 0, 0)";
+                    applyCSS({
+                        el: cache.wrapper,
+                        rule: 'transform',
+                        value: "translate3d(0, 0, 0)"
+                    });
                     setTimeout(utils.transition.on, 0);
                 }, (cache.interval / 2));
             }
 
             var newL = (cache.currentSlide - 1) * cache.sliderWidth;
-            cache.wrapper.style.webkitTransform = "translate3d(-" + (newL) + "px, 0, 0)";
+            applyCSS({
+                el: cache.wrapper,
+                rule: 'transform',
+                value: "translate3d(-" + (newL) + "px, 0, 0)"
+            });
             cache.currentSlide++;
         }
     };
